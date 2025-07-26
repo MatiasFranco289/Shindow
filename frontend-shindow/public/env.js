6d5d5c1bc1f620ef46ddc581de4ded662836a5ca
Deshens <matias.franco289@gmail.com>
2025-07-26 00:09:19 -0300

    updated dockerfile.dev in frontend for running the initial script and added generated .env to the .gitignore

--- frontend-shindow/public/env.js
+++
@@ -1,9 +0,0 @@
-window.env = {
-  NEXT_PUBLIC_SECRET: "same-secret-as-api",
-  NEXT_PUBLIC_KEY_FILE_MAX_SIZE: 10024,
-  NEXT_PUBLIC_TZ: "America/Argentina/Buenos_Aires",
-  NEXT_PUBLIC_BACK_BASE_URL: "http://locahost:5000",
-  NEXT_PUBLIC_API_BASE_URL: "http://localhost:5000/api",
-  NEXT_PUBLIC_INITIAL_PATH: "/",
-  NEXT_PUBLIC_CLIENT_BASE_URL: "http://localhost:3000"
-};
